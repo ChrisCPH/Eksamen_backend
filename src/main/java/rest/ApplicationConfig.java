9cfde6640b7429cd4abcc6f48446e95f5567c1c8
ChrisCPH <christoffer221199@hotmail.com>
2023-01-17 00:24:32 +0100

    First commit

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,6 +25,9 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(UserResource.class);
+        resources.add(ProjectResource.class);
+        resources.add(ProjectHourResource.class);
+        resources.add(DeveloperResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
